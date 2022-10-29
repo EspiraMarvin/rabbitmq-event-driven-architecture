c7cee2c08f28797ff8d79b3b170efae9d48eff65
marvin espira <espiramarvin@gmail.com>
2022-10-29 13:02:11 +0300

    creates channel and send buffer

--- orders-api/index.js
+++ orders-api/index.js
@@ -2,8 +2,28 @@ const express = require("express")
 const amqp = require("amqplib")
 const app = express()
 
-app.get('/', async (req, res) => {
+const amqpUrl = "amqp://localhost:5672"
+
+const orderData = {
+  customerId: 4,
+  orderId: 71,
+  number: 254791100001,
+}
+
+app.get("/", async (req, res) => {
+  try {
+    // create connection
+    const connection = await amqp.connect(amqpUrl)
+    // create channel
+    const channel = await connection.createChannel()
+    // create queque in the channel
+    await channel.assertQueue("order.shipped")
+    channel.sendToQueue("order.shipped", Buffer.from(JSON.stringify(orderData)))
     res.send("ORDERS API")
+  } catch (error) {
+    console.log(error)
+    // res.json(error)
+  }
 })
 
 app.listen(8000, () => {
